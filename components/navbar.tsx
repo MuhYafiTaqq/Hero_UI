d61efe2c03f67a6a3d125b109828eb328984dcce
MuhYafiTaqq <twizz1366@gmail.com>
2025-08-25 17:17:04 +0700

    nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,29 +20,33 @@ import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 
 export const Navbar = () => {
   const pathname = usePathname();
   const navItems = [
     { href: '/', key: 'home' },
-    { href: '/about', key: 'about' },
-    { href: '/blog', key: 'blog' },
-    { href: '/docs', key: 'docs' },
-    { href: '/pricing', key: 'pricing' },
+    { href: '/about', key: 'reels' },
+    { href: '/blog', key: 'template' },
+    { href: '/docs', key: 'faq' },
+    { href: '/pricing', key: 'donate' },
   ];
 
   return (
     <nav className="flex fixed w-full h-16 bottom-0 bg-black py-1 px-3">
       <div className="container mx-auto items-center justify-between h-full grid grid-cols-5">
         {navItems.map(item => (
-          <Link className="size-full" href={item.href} key={item.key}>
-            <Button
-              size="sm"
-              className={`w-full h-full ${pathname === item.href ? 'bg-white text-black' : 'bg-black text-white'}`}
-            >
+          <Link className={`size-full ${pathname === item.href ? 'bg-white text-black' : 'bg-black text-white hover:bg-white/20'} transition flex items-center justify-center rounded-lg flex-col p-0 gap-1`} href={item.href} key={item.key}>
+            <Image
+              src={`/NavBar-Icon/${item.key}${pathname === item.href ? "-active" : ""}.svg`}
+              alt={item.key}
+              width={24}
+              height={24}
+            />
+            <p className="text-xs">
               {item.key}
-            </Button>
+            </p>
           </Link>
         ))}
       </div>
